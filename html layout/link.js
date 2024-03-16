9ca73e494908d8f5273bacb2343e6e6e802bd38a
yallamma1223 <java63129@gmail.com>
2024-03-16 21:20:53 +0530

    test

---
+++ html layout/link.js
@@ -0,0 +1,12 @@
+
+function myfunction() {
+    document.getElementById("demo").style.color = "pink";
+    document.getElementById("demo").style.border = "2px solid blue";
+
+    document.getElementById("demo").style.textAlign = "center";
+
+    document.getElementById("demo").style.fontSize = "10px";
+
+
+}
+myfunction()
